f89a382c7e4150640c8a089b413b10478f6ae669
Sathira <sathiraumesh@gmail.com>
2018-10-30 22:44:21 +0530

    user mansge

--- services/admin_inventoryService.js
+++ services/admin_inventoryService.js
@@ -19,31 +19,6 @@ router.delete("/:id", inventoryController.deleteinventory);
 router.put("/:id", inventoryController.updateinventory);
 
 
-router.post("/add", function (req, res, next) {
-    var userData = req.body;
 
-    User.findOne({
-        username: req.body.username
-    }, function (err, user) {
-        if (err) {
-            res.json({
-                error: "internal server error"
-            });
-        } else if (user) {
-            res.json({
-                error: "user already exsist"
-            });
-        } else if (!user) {
-            var newUser = new User(userData);
-            newUser.save().then(function (user) {
-                res.send(user);
-            });
-            //res.json({success:"user sucseefuly added"});
-            //res.json(newU);
-        }
-
-    });
-    
-});
 
 module.exports = router;
