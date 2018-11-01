ec22efe94187db48d454c1d5939f105931ce5563
yashodhalakimini <yashodhalakimini31@gmail.com>
2018-11-02 01:31:14 +0530

    chage hall schemas.

--- models.js/hall.js
+++ models.js/hall.js
@@ -6,26 +6,26 @@ var hallSchema = mongoose.Schema({
         required: true
     },
 
-    halltype: {
-        lecturehall: {
-            type: String,
-            required: false
-        },
-        computerlab1: {
-            type: String,
-            required: false
-        },
-        computerlab2: {
-            type: String,
-            required: false
-        },
+    // halltype: {
+    //     lecturehall: {
+    //         type: String,
+    //         required: false
+    //     },
+    //     computerlab1: {
+    //         type: String,
+    //         required: false
+    //     },
+    //     computerlab2: {
+    //         type: String,
+    //         required: false
+    //     },
 
-        office: {
-            type: String,
-            required: false
-        }
+    //     office: {
+    //         type: String,
+    //         required: false
+    //     }
 
-    }
+    // }
 });
 
 var Hall = mongoose.model("Hall", hallSchema);
